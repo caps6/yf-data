a29f7b405cd65d2d1f3d3067068bb93e845877db
andrea <andrea.capitanelli@gmail.com>
2024-08-13 15:43:08 +0200

    fix: additional controls for missing data

--- yfdata/parsing.py
+++ yfdata/parsing.py
@@ -19,33 +19,43 @@ def parse_prices_or_rates(body: dict, ticker_or_pair: str) -> DataFrame:
 
     """
 
-    data = body["chart"]["result"][0]
+    df = None
 
-    metadata = data["meta"]
-    if metadata["instrumentType"] == "EQUITY" or metadata["instrumentType"] == "ETF":
-        code_name = "ticker"
-    elif metadata["instrumentType"] == "CURRENCY":
-        code_name = "pair"
-    else:
-        raise ValueError("Instrument type not supported.")
+    result = body["chart"]["result"]
 
-    if isinstance(data, dict) and "timestamp" in data and "indicators" in data:
+    if isinstance(result, list) and len(result) > 0:
 
-        ts = data["timestamp"]
-        quotes = data["indicators"]["quote"][0]
+        data = body["chart"]["result"][0]
 
-        df = DataFrame(
-            data={
-                "ts": ts,
-                "o": quotes["open"],
-                "h": quotes["high"],
-                "l": quotes["low"],
-                "c": quotes["close"],
-                "v": quotes["volume"],
-            }
-        )
+        metadata = data["meta"]
+        if (
+            metadata["instrumentType"] == "EQUITY"
+            or metadata["instrumentType"] == "ETF"
+        ):
+            code_name = "ticker"
+        elif metadata["instrumentType"] == "CURRENCY":
+            code_name = "pair"
+        else:
+            raise ValueError("Instrument type not supported.")
 
-    else:
+        if isinstance(data, dict) and "timestamp" in data and "indicators" in data:
+
+            ts = data["timestamp"]
+            quotes = data["indicators"]["quote"][0]
+
+            df = DataFrame(
+                data={
+                    "ts": ts,
+                    "o": quotes["open"],
+                    "h": quotes["high"],
+                    "l": quotes["low"],
+                    "c": quotes["close"],
+                    "v": quotes["volume"],
+                }
+            )
+
+    if df is None:
+        code_name = "instrument"
         df = DataFrame(
             data={
                 "ts": [],
@@ -100,15 +110,15 @@ def parse_financials(body: dict, ticker: str, freq: str, mapping: dict) -> DataF
                     dates.append(item["asOfDate"])
                     values.append(float(item["reportedValue"]["raw"]))
 
-            df = DataFrame(
-                data={
-                    "date": dates,
-                    "value": values,
-                }
-            )
+        df = DataFrame(
+            data={
+                "date": dates,
+                "value": values,
+            }
+        )
 
-            df["metric"] = mapping[yahoo_metric_name]
-            dfs.append(df)
+        df["metric"] = mapping[yahoo_metric_name]
+        dfs.append(df)
 
         df = pd.concat(dfs, ignore_index=True)
         df["ticker"] = ticker.lower()
@@ -131,22 +141,26 @@ def parse_dividends(body: dict, ticker: str) -> DataFrame:
 
     """
 
-    data = body["chart"]["result"][0]
-
     dates = []
     dividends = []
 
-    if isinstance(data, dict) and "events" in data:
+    result = body["chart"]["result"]
 
-        dict_dividends = data["events"]["dividends"]
+    if isinstance(result, list) and len(result) > 0:
 
-        for uts in dict_dividends:
+        data = result[0]
 
-            if "amount" in dict_dividends[uts]:
+        if isinstance(data, dict) and "events" in data:
 
-                ts = utils.timestamp2datetime(int(uts))
-                dates.append(ts)
-                dividends.append(dict_dividends[uts]["amount"])
+            dict_dividends = data["events"]["dividends"]
+
+            for uts in dict_dividends:
+
+                if "amount" in dict_dividends[uts]:
+
+                    ts = utils.timestamp2datetime(int(uts))
+                    dates.append(ts)
+                    dividends.append(dict_dividends[uts]["amount"])
 
     df = DataFrame(
         data={
